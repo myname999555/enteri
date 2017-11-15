3e7b98e17e3bc75dcd64c85af8dce4ac42ec275c
James Westerkamp <it@James-Westerkamps-iMac.local>
2017-11-14 21:03:04 -0600

    hiding old responses

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,2 +1,15 @@
 class Response < ApplicationRecord
+
+  def request
+    return Request.find(request_id)
+  end
+
+  def user
+    return User.find(user_id)
+  end
+
+  def item
+    item=Item.find(request.item_id) 
+  end
+
 end
